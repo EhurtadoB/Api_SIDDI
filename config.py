7269820c738f12e4bb798e42ec4c903d8f747b04
EhurtadoB <ehurtadobabilonia@gmail.com>
2024-07-17 19:31:01 -0500

    configuracion data base

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '1804')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'siddi_database')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'bbat5d5eu5hlaec1npct-mysql.services.clever-cloud.com')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'uakse15oumkncikp')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '5SHRsvCrIwzG4mQfey7n')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'bbat5d5eu5hlaec1npct')
     MYSQL_CURSORCLASS = 'DictCursor'
