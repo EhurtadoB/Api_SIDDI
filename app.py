bb003a8a9b2b83d004ebca4996de2dd22348494a
EhurtadoB <ehurtadobabilonia@gmail.com>
2024-07-21 21:24:13 -0500

    post imagen

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, send_file
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from config import Config
@@ -9,9 +9,10 @@ from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 from rembg import remove 
-
-
+import io
+from PIL import Image
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -104,7 +105,30 @@ def load_and_preprocess_image(filepath):
   img = img.astype('float32') / 255.0  # Normalizar la imagen
   return img
 
+class imagen(Resource):
+    def post(self):
+        imagen_path = request.files['imagen_path'] # Obtener el archivo de imagen enviado en la solicitud
+        if imagen_path and allowed_file(imagen_path.filename):
+            # Guardar el archivo de imagen en el directorio de uploads
+            filename = secure_filename(imagen_path.filename)
+            imagen_path.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #retorno la imagen
+            # Procesar la imagen
+            img = load_and_preprocess_image(f'uploads/{filename}')
 
+            # Convertir la imagen a un formato que se pueda enviar
+            img = (img * 255).astype(np.uint8)  # Desnormalizar la imagen
+            img = Image.fromarray(img)
+            img_io = io.BytesIO()
+            img.save(img_io, 'JPEG', quality=70)
+            img_io.seek(0)
+
+            return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='processed_image.jpg')
+            
+        else:
+            return {"message": "Invalid file or file extension not allowed"}, 400
+
+api.add_resource(imagen, '/imagen')
 
 class Infantes(Resource):
     def post(self):
